dadc2d007cab883beefc87ccf7d6d6e510eb1dd4
Bia Jorgensen <BiaJorgensen@users.noreply.github.com>
2022-12-28 19:46:32 -0600

    Created function that will show question

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,20 @@
-let question = document.getElementsByName("#question");
-let option = document.getElementsByName(".option");
+let question = document.querySelector("#question");
+let option = document.querySelectorAll(".option");
 
 let questionSet1 = {
     question: "How do you name a variable?",
-    option: ["let = variable", "variableName = name", "name = Variable", "var: name"],
-    answer: "let = variable"
+    options: ["let variable = name", "variableName = name", "name = Variable", "var: name"],
+    answer: "let variable = name"
 }
+
+function getQuestionSet(x) {
+    question.textContent = x.question;
+    option.forEach(function(element, index){
+        element.textContent = x.options [index];
+
+    })
+    
+    
+}
+
+getQuestionSet(questionSet1)
