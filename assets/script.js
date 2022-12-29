e897bc022c500e857575175b920a8959e62c45b6
Bia Jorgensen <BiaJorgensen@users.noreply.github.com>
2022-12-28 19:33:42 -0600

    Created first set of question in variable with objects

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,8 @@
 let question = document.getElementsByName("#question");
 let option = document.getElementsByName(".option");
 
+let questionSet1 = {
+    question: "How do you name a variable?",
+    option: ["let = variable", "variableName = name", "name = Variable", "var: name"],
+    answer: "let = variable"
+}
