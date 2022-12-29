ddd3289eeb807238c08427fc95372430122016bd
Bia Jorgensen <BiaJorgensen@users.noreply.github.com>
2022-12-28 20:05:53 -0600

    Created div in HTML to show if answer is correct or wrong; Created function to change said div depending on which answer user selects

--- assets/script.js
+++ assets/script.js
@@ -1,16 +1,25 @@
 let question = document.querySelector("#question");
 let option = document.querySelectorAll(".option");
+let evaluate = document.querySelector("#evaluate");
 
 let questionSet1 = {
     question: "How do you name a variable?",
     options: ["let variable = name", "variableName = name", "name = Variable", "var: name"],
-    answer: "let variable = name"
+    answer: 0
 }
 
 function getQuestionSet(x) {
     question.textContent = x.question;
     option.forEach(function(element, index){
         element.textContent = x.options [index];
+        element.addEventListener ('click', function(){
+            if(x.answer == index) {
+                evaluate.textContent = "Correct";
+            } 
+            else {
+                evaluate.textContent = "Wrong";
+            }
+        })
 
     })
     
