032939692c67e85460f40f7da6bb606273a95f8d
Bia Jorgensen <BiaJorgensen@users.noreply.github.com>
2022-12-28 21:12:21 -0600

    Modified variable with question to add more questions; Created function to show question and alternatives (one at a time); Previous code was not working for multiple questions, commented out, might delete it

--- assets/script.js
+++ assets/script.js
@@ -2,28 +2,58 @@ let question = document.querySelector("#question");
 let option = document.querySelectorAll(".option");
 let evaluate = document.querySelector("#evaluate");
 
-let questionSet1 = {
-    question: "How do you name a variable?",
-    options: ["let variable = name", "variableName = name", "name = Variable", "var: name"],
+let questions = [
+{
+    question: "1How do you name a variable?",
+    options: ["1let variable = name", "1variableName = name", "1name = Variable", "1var: name"],
     answer: 0
+},
+{
+    question: "2How do you name a variable?",
+    options: ["2let variable = name", "2variableName = name", "2name = Variable", "2var: name"],
+    answer: 0
+},
+{
+    question: "3How do you name a variable?",
+    options: ["3let variable = name", "3variableName = name", "3name = Variable", "3var: name"],
+    answer: 0   
 }
+]
 
-function getQuestionSet(x) {
-    question.textContent = x.question;
+function showQuestion() {
+    
+    question.textContent = questions[0].question;
     option.forEach(function(element, index){
-        element.textContent = x.options [index];
-        element.addEventListener ('click', function(){
-            if(x.answer == index) {
-                evaluate.textContent = "Correct";
-            } 
-            else {
-                evaluate.textContent = "Wrong";
-            }
-        })
+        element.textContent = questions[0].options[index]
 
     })
-    
-    
+
 }
 
-getQuestionSet(questionSet1)
+  showQuestion()  
+
+   
+    
+    
+
+
+
+// function getQuestionSet(x) {
+//     question.textContent = x.question;
+//     option.forEach(function(element, index){
+//         element.textContent = x.options [index];
+//         element.addEventListener ('click', function(){
+//             if(x.answer == index) {
+//                 evaluate.textContent = "Correct";
+//             } 
+//             else {
+//                 evaluate.textContent = "Wrong";
+//             }
+//         })
+
+//     })
+    
+    
+// }
+
+// getQuestionSet(questions)
