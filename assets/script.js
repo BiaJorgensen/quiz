37e97a7bb0c965072b43265e36b9306be1b60bb5
Bia Jorgensen <BiaJorgensen@users.noreply.github.com>
2023-01-02 16:18:56 -0600

    Removed 'Player' from high scores message

--- assets/script.js
+++ assets/script.js
@@ -160,7 +160,7 @@ function renderHighScores() {
      //Creates p elements for each user/score
      for (let i = 0; i < allUsers.length; i++) {
          let savedScores = document.createElement("p");
-         savedScores.textContent = (i +1) + " Player " + allUsers[i] + " - " + allScores[i] + " point(s)";
+         savedScores.textContent = (i +1) + " " + allUsers[i] + " - " + allScores[i] + " point(s)";
          highScoresDiv.appendChild(savedScores)
      }  
 }
@@ -222,7 +222,7 @@ hsLink.addEventListener('click', function() {
 //Creates p elements for each user/score
 for (let i = 0; i < allUsers.length; i++) {
     let savedScores = document.createElement("p");
-    savedScores.textContent = (i +1) + " Player " + allUsers[i] + " - " + allScores[i] + " point(s)";
+    savedScores.textContent = (i +1) + " " + allUsers[i] + " - " + allScores[i] + " point(s)";
     highScoresDiv.appendChild(savedScores)
 }  
 
